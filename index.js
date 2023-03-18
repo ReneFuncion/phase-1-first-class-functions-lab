9dad837ab3cf8d010f9b96afb3ee5698ae93b263
Rene Funcion <rcfun959@gmail.com>
2023-03-17 17:43:54 -0700

    Completed phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// function createFareMultiplier(outerfare) {
+//     return (innerfare) => innerfare * outerfare;
+// }
+const createFareMultiplier = (outerfare) => (innerfare) => innerfare * outerfare;
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversScuber, twoDrivers) {
+    return twoDrivers(driversScuber);
+}
